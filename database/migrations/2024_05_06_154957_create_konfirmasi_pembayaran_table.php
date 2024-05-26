3831417e6e1d0f048b90d3a86e4f8ded91ba09eb
Usuffff24 <usufodonk@gmail.com>
2024-05-26 17:05:08 +0800

    commit awal

--- database/migrations/2024_05_06_154957_create_konfirmasi_pembayaran_table.php
+++ database/migrations/2024_05_06_154957_create_konfirmasi_pembayaran_table.php
@@ -11,14 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('konfirmasi_pembayaran', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('foto')->nullable();
-            $table->rememberToken();
+            $table->foreignId('booking_id')->constrained('booking');
+            $table->string('atas_nama');
+            $table->string('bukti')->nullable();
+            $table->decimal('total', 10, 2);
             $table->timestamps();
         });
     }
@@ -28,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('konfirmasi_pembayaran');
     }
 };
