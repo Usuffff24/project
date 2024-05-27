7d46445e2a43146e4a7f3a08fb6133510ac86c69
Usuffff24 <usufodonk@gmail.com>
2024-05-28 01:02:43 +0800

    tampilan sementara

---
+++ resources/views/layout/index.blade.php
@@ -0,0 +1,277 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Borneo Futsal</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png') }}" rel="icon">
+  <link href="assets/img/apple-touch-icon.png'" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="{{ asset('assets/vendor/aos/aos.css') }}" rel="stylesheet">
+  <link href="{{ asset('assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+  <link href="{{ asset('assets/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
+  <link href="{{ asset('assets/vendor/boxicons/css/boxicons.min.css') }}" rel="stylesheet">
+  <link href="{{ asset('assets/vendor/glightbox/css/glightbox.min.css') }}" rel="stylesheet">
+  <link href="{{ asset('assets/vendor/remixicon/remixicon.css') }}" rel="stylesheet">
+  <link href="{{ asset('assets/vendor/swiper/swiper-bundle.min.css') }}" rel="stylesheet">
+  
+  <!-- Template Main CSS File -->
+  <link href="{{ asset('assets/css/style.css') }}" rel="stylesheet">
+  <!-- =======================================================
+  * Template Name: Gp
+  * Template URL: https://bootstrapmade.com/gp-free-multipurpose-html-bootstrap-template/
+  * Updated: Mar 17 2024 with Bootstrap v5.3.3
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top ">
+    <div class="container d-flex align-items-center justify-content-lg-between">
+      <h1><a></a></h1>
+      
+      <!-- Uncomment below if you prefer to use an image logo -->
+      <!-- <a href="index.html" class="logo me-auto me-lg-0"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+
+      <nav id="navbar" class="navbar order-last order-lg-0">
+        <ul>
+          <li><a style="font-size: 20px;" class="nav-link scrollto active" href="#hero">Beranda</a></li>
+          <li><a style="font-size: 20px;" class="nav-link scrollto" href="#about">Profile</a></li>
+          <li><a style="font-size: 20px;" class="nav-link scrollto" href="#services">Booking</a></li>
+          <li><a style="font-size: 20px;" class="nav-link scrollto" href="#contact">Kontak</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+      <a href="#about" class="get-started-btn scrollto">Login</a>
+
+    </div>
+  </header><!-- End Header -->
+  <!-- ======= Hero Section ======= -->
+<section id="hero" class="d-flex align-items-center justify-content-center" style="background-image: url('{{ asset('assets/img/lapangan.jpg') }}');">
+  <img src="assets/img/logoborneo1.png" alt="Logo">
+    <div class="container" data-aos="fade-up">
+      <div class="row justify-content-center" data-aos="fade-up" data-aos-delay="150">
+        <div class="col-xl-100 col-lg-15">
+          <h1 style="font-size: 70px;">BORNEO INDOOR FUTSAL</h1>
+          <h2 style="font-size: 30px;">SELAMAT DATANG DI WEBSITE BORNEO INDOOR FUTSAL</h2>
+        </div>
+      </div>
+    </div>
+  </section><!-- End Hero -->
+  
+
+  <main id="main">
+
+    <!-- ======= About Section ======= -->
+    <section id="about" class="about">
+      <div class="container" data-aos="fade-up">
+
+        <div class="row">
+          <div class="col-lg-6 order-1 order-lg-2" data-aos="fade-left" data-aos-delay="100">
+            <<img src="assets/img/profile.jpg" class="img-fluid" alt="">
+          </div>
+          <div class="col-lg-6 pt-4 pt-lg-0 order-2 order-lg-1 content" data-aos="fade-right" data-aos-delay="100">
+            <h3>Borneo Futsal Menjadi Lapangan Terbaik Di Banjarmasin.</h3>
+            <p class="fst-italic">
+             Borneo Futsal Pernah Mendapatkan Predikat Terbaik
+            </p>
+            <ul>
+              <li><i class="ri-check-double-line"></i> Penghargaan Dari Dinas Kementrian Olahraga Banjarmasin.</li>
+              <li><i class="ri-check-double-line"></i> Penghargaan Dari Gubernur Kalimantan Selatan.</li>
+              <li><i class="ri-check-double-line"></i> Penghargaan Dari Walikota Banjarmasin.</li>
+            </ul>
+            <p>
+              Itulah alasan mengapa lapangan Borneo Indoor Futsal menjadi salah satu lapangan terbaik dibanjarmasin bahkan
+              kalimantan selatan
+            </p>
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End About Section -->
+
+    <!-- ======= Services Section ======= -->
+    <section id="services" class="services">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+          <h2>BOOKING</h2>
+          <p>Pilih Lapangan</p>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-4 col-md-6 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
+            <div class="icon-box">
+              <img src="assets/img/lapangan1.jpg" alt="Lapangan" style="width: 250px; height: 200px; object-fit: cover;margin-bottom: 15px;">
+              <h4><a href="">LAPANGAN 1</a></h4>
+              <p>Lapangan 1 Borneo Indoor Futsal</p>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-md-0" data-aos="zoom-in" data-aos-delay="200">
+            <div class="icon-box">
+              <img src="assets/img/lapangan2.jpg" alt="Lapangan" style="width: 250px; height: 200px; object-fit: cover;margin-bottom: 15px;">
+              <h4><a href="">LAPANGAN 2</a></h4>
+              <p>Lapangan 2 Borneo Indoor Futsal</p>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-lg-0" data-aos="zoom-in" data-aos-delay="300">
+            <div class="icon-box">
+              <img src="assets/img/lapangan3.jpg" alt="Lapangan" style="width: 250px; height: 200px; object-fit: cover;margin-bottom: 15px;">
+              <h4><a href="">LAPANGAN 3</a></h4>
+              <p>Lapangan 3 Borneo Indoor Futsal</p>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4" data-aos="zoom-in" data-aos-delay="100">
+            <div class="icon-box">
+              <img src="assets/img/lapangan4.jpg" alt="Lapangan" style="width: 250px; height: 200px; object-fit: cover;margin-bottom: 15px;">
+              <h4><a href="">LAPANGAN 4</a></h4>
+              <p>Lapangan 4 Borneo Indoor Futsal</p>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4" data-aos="zoom-in" data-aos-delay="200">
+            <div class="icon-box">
+              <img src="assets/img/badminton1.jpg" alt="Lapangan" style="width: 250px; height: 200px; object-fit: cover;margin-bottom: 15px;">
+              <h4><a href="">BADMINTON 1</a></h4>
+              <p>BADMINTON 1 Borneo Indoor Futsal</p>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4" data-aos="zoom-in" data-aos-delay="300">
+            <div class="icon-box">
+              <img src="assets/img/badminton2.jpg" alt="Lapangan" style="width: 250px; height: 200px; object-fit: cover;margin-bottom: 15px;">
+              <h4><a href="">BADMINTON 2</a></h4>
+              <p>BADMINTON 2 Borneo Indoor Futsal</p>
+            </div>
+          </div>
+
+          <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 text-center" data-aos="zoom-in" data-aos-delay="300">
+                    <div class="icon-box">
+                        <img src="assets/img/tenismeja.jpg" alt="Lapangan" style="width: 250px; height: 200px; object-fit: cover; margin-bottom: 15px;">
+                        <h4><a href="">TENIS MEJA</a></h4>
+                        <p>Tenis Meja Borneo Indoor Futsal</p>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 text-center" data-aos="zoom-in" data-aos-delay="300">
+                    <div class="icon-box">
+                        <img src="assets/img/billiard1.jpg" alt="Lapangan" style="width: 250px; height: 200px; object-fit: cover; margin-bottom: 15px;">
+                        <h4><a href="">BILLIARD</a></h4>
+                        <p>Billiard Borneo Indoor Futsal</p>
+                    </div>
+                </div>
+            </div>
+        </div>        
+        </div>
+
+      </div>
+    </section><!-- End Services Section -->
+    <!-- ======= Contact Section ======= -->
+    <section id="contact" class="contact">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+          <h2>Kontak</h2>
+          <p>Lokasi</p>
+        </div>
+
+        <div>
+          <iframe style="border:0; width: 100%; height: 270px;" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7978.370414178293!2d114.59253938558868!3d-3.311171896373741!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x32dc14446db4a3f3%3A0x8b88f1d6ae0cfd32!2sPHCG%2B3G9%2C%20Alalak%20Utara%2C%20Kec.%20Banjarmasin%20Utara%2C%20Kota%20Banjarmasin%2C%20Kalimantan%20Selatan%2070124!5e0!3m2!1sen!2sid!4v1622090151197!5m2!1sen!2sid" frameborder="0" allowfullscreen></iframe>
+        </div>
+
+        <div class="row mt-5">
+
+          <div class="col-lg-4">
+            <div class="info">
+              <div class="address">
+                <i class="bi bi-geo-alt"></i>
+                <h4>Lokasi:</h4>
+                <p>Jl. HKSN Komp. Amd Permai Jl. HKSN, Alalak Utara, 
+                  Kec. Banjarmasin Utara, Kota Banjarmasin, Kalimantan Selatan 70127</p>
+              </div>
+
+              <div class="email">
+                <i class="bi bi-envelope"></i>
+                <h4>Email:</h4>
+                <p>afpkalsel@gmail.com</p>
+              </div>
+
+              <div class="phone">
+                <i class="bi bi-phone"></i>
+                <h4>Telepon:</h4>
+                <p>+6281348486269</p>
+              </div>
+            </div>
+            <div class="social-links mt-3" style="font-size: 30px;">
+              <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+              <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+              <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+          </div>          
+          </div>
+
+          <div class="col-lg-8 mt-5 mt-lg-0">
+
+            <form action="forms/contact.php" method="post" role="form" class="php-email-form">
+              <div class="row">
+                <div class="col-md-6 form-group">
+                  <input type="text" name="name" class="form-control" id="name" placeholder="Masukan Nama" required>
+                </div>
+                <div class="col-md-6 form-group mt-3 mt-md-0">
+                  <input type="email" class="form-control" name="email" id="email" placeholder="Masukan Email" required>
+                </div>
+              </div>
+              <div class="form-group mt-3">
+                <input type="text" class="form-control" name="subject" id="subject" placeholder="Keterangan" required>
+              </div>
+              <div class="form-group mt-3">
+                <textarea class="form-control" name="message" rows="5" placeholder="Pesan" required></textarea>
+              </div>
+              <div class="my-3">
+                <div class="loading">Loading</div>
+                <div class="error-message"></div>
+                <div class="sent-message">Your message has been sent. Thank you!</div>
+              </div>
+              <div class="text-center"><button type="submit">Jika Mau Booking Untuk Pertandingan Bisa Isi Data</button></div>
+            </form>
+
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Contact Section -->
+  <div id="preloader"></div>
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+<script src="{{ asset('assets/vendor/purecounter/purecounter_vanilla.js') }}"></script>
+<script src="{{ asset('assets/vendor/aos/aos.js') }}"></script>
+<script src="{{ asset('assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ asset('assets/vendor/glightbox/js/glightbox.min.js') }}"></script>
+<script src="{{ asset('assets/vendor/isotope-layout/isotope.pkgd.min.js') }}"></script>
+<script src="{{ asset('assets/vendor/swiper/swiper-bundle.min.js') }}"></script>
+<script src="{{ asset('assets/vendor/php-email-form/validate.js') }}"></script>
+
+<!-- Template Main JS File -->
+<script src="{{ asset('assets/js/main.js') }}"></script>
+
+</body>
+</html>
