b34535ea5f0b9fee491b4a09ea2fb4f20857ea56
Usuffff24 <usufodonk@gmail.com>
2024-05-28 01:10:50 +0800

    tampilan sementara

--- resources/views/layout/index.blade.php
+++ resources/views/layout/index.blade.php
@@ -231,11 +231,14 @@
 
             <form action="forms/contact.php" method="post" role="form" class="php-email-form">
               <div class="row">
-                <div class="col-md-6 form-group">
+                <div class="col-md-4 form-group">
                   <input type="text" name="name" class="form-control" id="name" placeholder="Masukan Nama" required>
                 </div>
-                <div class="col-md-6 form-group mt-3 mt-md-0">
+                <div class="col-md-4 form-group mt-3 mt-md-0">
                   <input type="email" class="form-control" name="email" id="email" placeholder="Masukan Email" required>
+                </div>
+                <div class="col-md-4 form-group mt-3 mt-md-0">
+                  <input type="email" class="form-control" name="email" id="email" placeholder="Masukan No.Telepon" required>
                 </div>
               </div>
               <div class="form-group mt-3">
